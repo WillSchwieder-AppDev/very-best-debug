8bb84560da177a00e61e6d2cba358065500c6519
WillSchwieder <wschwied@chicagobooth.edu>
2022-02-22 15:24:41 +0000

    Finished up

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -16,13 +16,15 @@ class VenuesController < ApplicationController
   end
 
   def create
-    @venue = Venue.new
+    venue = Venue.new
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("name")
-    venue.neighborhood = params.fetch("neighborhood")
+    venue.name = params.fetch("query_name")
+    venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
 
-    redirect_to("/venues/#{venue.name}")
+    @the_venue = venue
+
+    redirect_to("/venues/"+venue.id.to_s)
   end
   
   def update
